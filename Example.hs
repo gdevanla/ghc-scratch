5d7defa0a49ce34b667a6f5f61fc02b3ebf5a741
grdvnl <grdvnl@gmail.com>
2020-10-09 17:59:20 -0700

    Prototype to get Record function and Type

---
+++ Example.hs
@@ -0,0 +1,8 @@
+module Example where
+
+--data Animal = Cat | Dog
+
+data Fund = Fund1 {z:: Integer, y:: String}
+
+--add :: Int -> Int -> Int
+--add x y = x + y
--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Main where
 
 -- import GHC (
@@ -6,6 +8,7 @@ import GHC
 import GHC.Paths
 import GHC.LanguageExtensions
 import DynFlags
+import Outputable (Outputable, showPpr)
 
 import Lib
 import Data.Dynamic (fromDynamic)
@@ -14,6 +17,17 @@ import Unsafe.Coerce (unsafeCoerce)
 import Control.Exception
 import System.Console.Haskeline
 import qualified Data.Text as T
+
+banner :: MonadIO m => String -> m ()
+banner msg =
+  liftIO $
+    putStrLn
+      ( (replicate (fromIntegral n) '=')
+          ++ msg
+          ++ (replicate (fromIntegral n) '=')
+      )
+  where
+    n = (76 - length msg) `div` 2
 
 
 initSession :: IO HscEnv
@@ -81,9 +95,78 @@ repl env = do
       env' <- ghcCatch (session env (eval input))
       maybe (repl env) repl env'
 
+-- main :: IO ()
+-- main = do
+--   print "Strting session"
+--   env <- initSession
+--   runInputT defaultSettings (repl env)
+
+showGhc :: (Outputable a) => a -> String
+showGhc = showPpr unsafeGlobalDynFlags
+
 
 main :: IO ()
-main = do
-  print "Strting session"
-  env <- initSession
-  runInputT defaultSettings (repl env)
+main = runGhc (Just libdir) $ do
+  env <- getSession
+  dflags <- getSessionDynFlags
+  setSessionDynFlags $ dflags { hscTarget = HscInterpreted }
+
+  target <- guessTarget "Example.hs" Nothing
+  setTargets [target]
+  load LoadAllTargets
+  modSum <- getModSummary $ mkModuleName "Example"
+
+  pmod <- parseModule modSum      -- ModuleSummary
+  tmod <- typecheckModule pmod    -- TypecheckedSource
+  dmod <- desugarModule tmod      -- DesugaredModule
+  let core = coreModule dmod      -- ModGuts
+  --stg <- liftIO $ coreToStg dflags (mg_module core) (mg_binds core)
+
+  liftIO $ banner "Parsed Source"
+  liftIO $ putStrLn $ showGhc (parsedSource pmod)
+
+  let hsmodule = unLoc (parsedSource pmod)
+  liftIO $ putStrLn $ showGhc hsmodule
+
+  liftIO $ putStrLn $ showGhc (hsmodDecls hsmodule)
+
+  let decl = unLoc $ head $ (hsmodDecls hsmodule)
+  liftIO $ putStrLn $ showGhc decl
+
+  case decl of
+    TyClD _ (DataDecl{..}) -> do
+      case tcdDataDefn of
+        --HsDataDefn {..} -> liftIO $ putStrLn $ showGhc $ unLoc $ head dd_cons
+        HsDataDefn {..} -> do
+          case (unLoc. head $ dd_cons) of
+            ConDeclGADT {..} -> liftIO $ putStrLn $ showGhc $ con_names
+            ConDeclH98 {..} -> do
+              case con_args of
+                -- RecCon fld -> liftIO $ putStrLn $ showGhc $ unLoc <$> unLoc fld
+                RecCon fld ->
+                  case unLoc . head . unLoc $ fld of
+                    ConDeclField {..} -> liftIO $ putStrLn $ showGhc $ unLoc cd_fld_type
+                    otherwise -> liftIO $ putStrLn "XXConfField"
+                otherwise -> liftIO $ putStrLn "not record"
+        otherwise -> liftIO $ putStrLn "----"
+
+      liftIO $ putStrLn $ showGhc $ tcdDataDefn
+    _ -> liftIO $ putStrLn "nothing here"
+
+  -- liftIO $ banner "Renamed Module"
+  -- liftIO $ putStrLn $ showGhc ( tm_renamed_source tmod )
+
+  -- liftIO $ banner "Typechecked Module"
+  -- liftIO $ putStrLn $ showGhc ( tm_typechecked_source tmod )
+
+  -- liftIO $ banner "Typed Toplevel Definitions"
+  -- liftIO $ putStrLn $ showGhc ( modInfoTyThings (moduleInfo tmod) )
+
+  -- liftIO $ banner "Typed Toplevel Exports"
+  -- liftIO $ putStrLn $ showGhc ( modInfoExports (moduleInfo tmod) )
+
+  --liftIO $ banner "Core Module"
+  --liftIO $ putStrLn $ showGhc ( mg_binds core )
+
+  --liftIO $ banner "STG"
+  --liftIO $ putStrLn $ showGhc stg
